ca49ab837f9b28f8a2168fc7a0496f989f12cbf9
Naaman Newbold <naaman@heroku.com>
2014-01-16 17:44:19 -0800

    unused

--- main.go
+++ main.go
@@ -124,51 +124,6 @@ func (s *Slug) parseProcfile() *pf.Procfile {
 	return procfile
 }
 
-const executableFlag = 0111
-
-func executable(filemode os.FileMode) bool {
-	e := filemode & executableFlag
-	return e != 0
-}
-
-func recursiveCopy(from string, to string) filepath.WalkFunc {
-	toPath := to + "/"
-
-	return func(path string, info os.FileInfo, err error) error {
-		src, err := os.Open(path)
-
-		if err != nil {
-			return err
-		}
-
-		defer src.Close()
-
-		dstPath := strings.Replace(path, from, toPath, 1)
-
-		if info.IsDir() {
-			os.MkdirAll(dstPath, info.Mode())
-		} else {
-			dst, err := os.Create(dstPath)
-			if err != nil {
-				return err
-			}
-			defer dst.Close()
-			_, err = io.Copy(dst, src)
-			return err
-		}
-
-		return nil
-	}
-}
-
-func scriptIsValid(script string) bool {
-	scriptStat, err := os.Stat(script)
-	if err != nil || !executable(scriptStat.Mode()) {
-		return false
-	}
-	return true
-}
-
 func handleError(_e error) {
 	if _e != nil {
 		log.Fatal(_e)
